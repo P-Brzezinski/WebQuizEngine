e14669b8885cae9ef2fe8dac9cd47cc0815b6e80
Pawel Brzezinski <pawelbrzezinski1987@gmail.com>
2020-11-12 21:13:51 +0100

    Add first simple POST request

--- src/main/java/pl/brzezinski/web_quiz_service/controller/QuizController.java
+++ src/main/java/pl/brzezinski/web_quiz_service/controller/QuizController.java
@@ -1,7 +1,10 @@
 package pl.brzezinski.web_quiz_service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pl.brzezinski.web_quiz_service.model.Answer;
 import pl.brzezinski.web_quiz_service.model.Quiz;
 
 import java.util.ArrayList;
@@ -13,12 +16,20 @@ public class QuizController {
     private List<Quiz> quizList = new ArrayList<>();
 
     public QuizController() {
-        quizList.add(new Quiz("The Java Logo", "What is depicted on the Java logo?", new String[]{"Robot", "Tea leaf", "Cup of coffee", "Bug"
-        }));
+        quizList.add(new Quiz("The Java Logo", "What is depicted on the Java logo?", new String[]{"Robot", "Tea leaf", "Cup of coffee", "Bug"}));
     }
 
     @GetMapping(path = "/api/quiz")
     public Quiz getQuiz() {
         return quizList.get(0);
+    }
+
+    @PostMapping(path = "/api/quiz")
+    public Answer getAnswer(@RequestParam(name = "answer") int answer){
+        if (answer == 2){
+            return new Answer(true, "Congratulations, you're right!");
+        }else {
+            return new Answer(false, "Wrong answer! Please, try again.");
+        }
     }
 }
---
+++ src/main/java/pl/brzezinski/web_quiz_service/model/Answer.java
@@ -0,0 +1,31 @@
+package pl.brzezinski.web_quiz_service.model;
+
+public class Answer {
+
+    private boolean success;
+    private String feedback;
+
+    public Answer() {
+    }
+
+    public Answer(boolean success, String feedback) {
+        this.success = success;
+        this.feedback = feedback;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+}
