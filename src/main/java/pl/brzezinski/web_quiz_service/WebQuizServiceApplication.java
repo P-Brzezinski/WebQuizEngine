12bf210f5e8cfb3558b933e51de2d06b469bb796
Pawel Brzezinski <pawelbrzezinski1987@gmail.com>
2020-11-12 20:23:33 +0100

    First commit - project init

---
+++ src/main/java/pl/brzezinski/web_quiz_service/WebQuizServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.brzezinski.web_quiz_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebQuizServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebQuizServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/brzezinski/web_quiz_service/WebQuizServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.brzezinski.web_quiz_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebQuizServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
