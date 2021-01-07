4c68839b3d02d156140ba4d42fd20810666ba03a
P-Brzezinski <pawelbrzezinski1987@gmail.com>
2021-01-07 16:25:03 +0100

    Enable Async

--- src/main/java/pl/brzezinski/web_quiz_service/WebQuizServiceApplication.java
+++ src/main/java/pl/brzezinski/web_quiz_service/WebQuizServiceApplication.java
@@ -2,8 +2,10 @@ package pl.brzezinski.web_quiz_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class WebQuizServiceApplication {
 
 	public static void main(String[] args) {
